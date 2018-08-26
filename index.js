f21ecd36783eb2f451ddacdb17d0a601d0cab05f
jokebookservice1 <jokebookyeye@gmail.com>
2018-08-26 23:13:39 +0300

    Re-write module to allow for multiple same directive types

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-module.exports = function (options) {
+module.exports = function (fields) {
+  if(!Object.keys(fields).map(normalizeCase).includes("Contact"))
+    throw "You must include a `contact` property for security.txt";
+  
+  const securityTxt = []
+
+  for(const directive in fields) {
+    const key = normalizeCase(directive)
+    
+    for(const value of forceToArray(fields[directive]) {
+      securityTxt.push(`${key}: ${value}`)
+    }
+  }
+  
   return function (request, response) {
-    const securityTxt = Object.keys(options).map(option => {
-      
-    })
-    response.send(
-      `Contact: ${options.contact}\nEncryption: ${options.encryption}\nAcknowledgements: ${options.acknowledgements}`
-    )
+    response.send(securityTxt.join("\n"))
+  }
+}
+
+/**
+ * Internal function to convert a string to one where the first
+ * character is uppercase, and all subsequent are lowercase.
+ *
+ * @param {string} string - The string to convert
+ * @return {string} The capitalized form of the string
+ */
+function normalizeCase(string) {
+  return string[0].toUpperCase() + string.substr(1).toLowerCase();
+}
+
+/**
+ * Internal function. Returns arrays as passed, but converts some
+ * string X into an array [X] of length 1. For example, "abc" would
+ * become ["abc"].
+ *
+ * @param {string|array} stringOrArray - The string or array to force to an array
+ * @return {array} The array form of the argument
+ */
+function forceToArray(stringOrArray) {
+  if(Array.isArray(stringOrArray)) {
+    return stringOrArray
+  } else {
+    return [stringOrArray]
   }
 }
